8f9dd5f05e2e0ac9cda41ac769dbc1edc3a27460
Jerome Russ <jerome.d.russ+github@gmail.com>
2016-07-22 01:20:31 -0500

    Fixing issue with multiple windows opening

--- scripts/background.js
+++ scripts/background.js
@@ -3,19 +3,29 @@
 var socket = io.connect('http://soytap.azurewebsites.net/');
 //var socket = io.connect('http://localhost:3000/');
 
+var lastId;
 socket.on('refresh', function (data) {
   console.log(data);
 });
 socket.on('new item', function (data) {
   console.log(data);
+  var url = "http://rtc.nwie.net/jazz/web/projects/Dev%20Center#action=com.ibm.team.workitem.viewWorkItem&id=" + data.card
+  if(data.toast){
+    window.open(url);
+  }
   var options = {
     type: "basic",
     title: "New Notification",
     message: JSON.stringify(data),
     iconUrl: "/images/icon__c.png"
   }
-  chrome.notifications.onClicked.addListener(function (id){
-    window.open("http://rtc.nwie.net/jazz/web/projects/Dev%20Center#action=com.ibm.team.workitem.viewWorkItem&id=" + id);
-  });
+  lastId = data.card;
   chrome.notifications.create(data.card, options, null);
+});
+
+chrome.notifications.onClicked.addListener(function (id){
+  if(lastId == id){
+    var url = "http://rtc.nwie.net/jazz/web/projects/Dev%20Center#action=com.ibm.team.workitem.viewWorkItem&id=" + id
+    window.open(url);
+  }
 });
