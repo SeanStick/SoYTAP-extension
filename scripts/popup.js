fd12bee6e0728d6b05dfd2bd4d75bd5d2874f091
Jerome Russ <jerome.d.russ+github@gmail.com>
2016-07-22 01:56:12 -0500

    Adding feed for events

--- scripts/popup.js
+++ scripts/popup.js
@@ -5,6 +5,15 @@ var socket = io.connect('http://soytap.azurewebsites.net/');
 
 socket.on('refresh', function (data) {
   console.log(data);
+  var feed = document.getElementById('feed');
+  feed.innerHTML = '';
+  for(var i=0; i < data.length; i++){
+    console.log(data[i]);
+    if(data[i].type == 'RTC'){
+      var url = "http://rtc.nwie.net/jazz/web/projects/Dev%20Center#action=com.ibm.team.workitem.viewWorkItem&id=" + data[i].card;
+      feed.innerHTML += '<a href=' + url + ' target="_blank">' + data[i].card + '</a><hr/>'
+    }
+  }
 });
 socket.on('new rtc item', function (data) {
   console.log(data);
